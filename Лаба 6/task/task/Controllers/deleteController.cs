5172b0c86e5dada183a00ff70aa28db7f45aa2ac
Luverk1d <arnfourm@gmail.com>
2025-05-14 13:16:52 +0300

    lab 6 хоть что то

---
+++ Лаба 6/task/task/Controllers/deleteController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using OrderModel.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace task_beta.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class deleteController : Controller
+    {
+        private string connection;
+        
+        public deleteController(IConfiguration configuration)
+        {
+            connection = configuration.GetConnectionString("MySqlConnection");
+        }
+
+        public Order order = new Order();
+
+        [HttpGet]
+        public IActionResult Deleter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DeleterPost(Order order)
+        {
+            string sqlCommand = "DELETE FROM orderinfo WHERE OrderId = @idOrder";
+
+            using (MySqlConnection connectionCurrent = new MySqlConnection(connection))
+            {
+                connectionCurrent.Open();
+                MySqlCommand commandCurrent = new MySqlCommand(sqlCommand, connectionCurrent);
+                commandCurrent.Parameters.AddWithValue("@idOrder", order.IdOrder);
+
+                commandCurrent.ExecuteNonQuery();
+                connectionCurrent.Close();
+            }
+
+            return RedirectToAction("Deleter", "Delete");
+        }
+    }
+}
---
+++ Лаба 6/task/task/Controllers/homeController.cs
@@ -0,0 +1,103 @@
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using OrderModel.Models;
+using ProductModel.Models;
+
+namespace task.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private string connection;
+
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+
+        {
+            _logger = logger;
+            connection = configuration.GetConnectionString("MySqlConnection");
+        }
+
+        private List<Product> products = new List<Product>();
+
+        [BindProperty]
+        public Order order { get; set; } = new Order();
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            products.Clear();
+
+            string sqlCommandProduct = "select * from product";
+            using (MySqlConnection connectionCurrent = new MySqlConnection(connection))
+            {
+                connectionCurrent.Open();
+                MySqlCommand commandCurrent = new MySqlCommand(sqlCommandProduct, connectionCurrent);
+                using (var sqlSelectProduct = commandCurrent.ExecuteReader())
+                {
+                    while (sqlSelectProduct.Read())
+                    {
+                        products.Add(new Product
+                        {
+                            IdProduct = sqlSelectProduct.GetInt32("ProductId"),
+                            NameProduct = sqlSelectProduct.GetString("ProductName"),
+                            CostProduct = sqlSelectProduct.GetInt32("ProductCost"),
+                            imagePathProduct = sqlSelectProduct.GetString("ProductIMG")
+                        });
+                    }
+                }
+            }
+
+            ViewBag.Products = products;
+            ViewBag.succesValid = successValid;
+            ViewBag.errorValid = errorValid;
+
+            return View();
+        }
+
+        private string successValid = "";
+        private string errorValid = "";
+
+        [HttpPost]
+        public IActionResult Order(Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                errorValid = "Некоторые данные были введены неправильно!";
+                return RedirectToAction("Index");
+            }
+
+            string sqlCommand = "insert into orderinfo (OrderName, OrderSurname, OrderEmail, OrderAddress, OrderQuantity)" +
+                                "values (@NameOrder, @SecondNameOrder,  @EmailOrder, @AddressOrder, @QuantityOrder); ";
+            using (MySqlConnection connectionCurrent = new MySqlConnection(connection))
+            {
+                connectionCurrent.Open();
+                MySqlCommand commandCurrent = new MySqlCommand(sqlCommand, connectionCurrent);
+                commandCurrent.Parameters.AddWithValue("@NameOrder", order.NameOrder);
+                commandCurrent.Parameters.AddWithValue("@SecondNameOrder", order.SecondNameOrder);
+                commandCurrent.Parameters.AddWithValue("@EmailOrder", order.EmailOrder);
+                commandCurrent.Parameters.AddWithValue("@AddressOrder", order.AddressOrder);
+                commandCurrent.Parameters.AddWithValue("@QuantityOrder", order.QuantityOrder);
+
+                commandCurrent.ExecuteNonQuery();
+                connectionCurrent.Close();
+            }
+
+            successValid = $"Спасибо {order.NameOrder} {order.SecondNameOrder}, заказ выполнен успешно!{Environment.NewLine}" +
+               $"Доставим товар по адресу {order.AddressOrder} в количестве {order.QuantityOrder} шт.{Environment.NewLine}" +
+               $"Электронный чек придет в ближайшее время на почту {order.EmailOrder}";
+
+            return RedirectToAction("Index");
+        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
